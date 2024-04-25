ea1ac0a70ec30b24efeee14967156a669d50b047
Carlos F Arboleda G <arbofercho@gmail.com>
2024-04-24 23:40:52 -0500

    Testing Llama3 model

--- examples/gemma-ollama/index.js
+++ examples/gemma-ollama/index.js
@@ -6,9 +6,17 @@ const accounts = {
   AFC: "Cuenta banco 3",
 };
 
+const getPrompt = (text) => {
+  return `Valid source and destination accounts are: C2163, C1408, AFC. Do not encode the accounts.
+  ${text}`;
+};
+
 const response = await ollama.generate({
-  model: "expenses-extractor:gemma2b",
-  prompt: `Vuelos a Madrid para luna de miel por 2500000, C2163`,
+  model: "expenses-extractor:llama3",
+  // prompt: getPrompt(`Vuelos a Madrid para luna de miel por 2500000, C2163`),
+  // prompt: getPrompt("Transferencia pago TC de C1408 a C2163 por 1000000"),
+  // prompt: getPrompt("Pay Masari administrative fee by 301000, C1408"),
+  prompt: getPrompt("Ingresos por intereses 1200, AFC"),
   stream: false,
 });
 
@@ -17,7 +25,9 @@ const response = await ollama.generate({
 const json = JSON.parse(response.response);
 const transaction = {
   ...json,
-  account: accounts[json.account],
+  sourceAccountO: json.sourceAccount && accounts[json.sourceAccount],
+  destinationAccountO:
+    json.destinationAccount && accounts[json.destinationAccount],
   createAt: new Date(),
 };
 
